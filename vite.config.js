58dcc604f303f91c7ee64272ab45f8cd5419b21f
Alex Harding <alexharding@rti.org>
2022-08-30 13:01:20 -0400

    adding library build target

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  // https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/harness.js"),
+      name: "harness-vue",
+      fileName: (format) => `harness-vue.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
