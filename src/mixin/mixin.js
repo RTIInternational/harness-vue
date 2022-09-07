a629192ed01666952f760f24d602e3891749d3e1
Alex Harding <alexharding@rti.org>
2022-09-07 14:45:45 -0400

    requiring the pinia instance be passed to the mixin

--- src/mixin/mixin.js
+++ src/mixin/mixin.js
@@ -1,5 +1,6 @@
 import getActions from "../store/actions";
 import getState from "../store/state";
+import getGetters from "../store/getters";
 import getValidationFunctions from "../store/validation";
 import getFilterHelperFunctions from "../store/filters";
 import getChartHelperFunctions from "../store/charts";
@@ -7,52 +8,58 @@ import getDataHelperFunctions from "../store/data";
 import harnessStore from "../store/harnessStore";
 import { mapActions, mapState } from "pinia";
 
-export default {
-  beforeCreate() {
-    // if route name is a valid harness page, use it as waypoint
-    let waypoint = false;
-    const harnessMetadata = harnessStore();
+export default function mixin(pinia) {
+  return {
+    beforeCreate() {
+      // if route name is a valid harness page, use it as waypoint
+      let waypoint = false;
 
-    if (
-      this.$route &&
-      this.$route.name &&
-      harnessMetadata.getPages.includes(this.$route.name)
-    ) {
-      waypoint = this.$route.name;
-    }
-    // if router is not installed and only a single harness page exists, use it as waypoint
-    if ((harnessMetadata.getPages.length === 1) & !this.$route) {
-      waypoint = harnessMetadata.pages[0];
-    }
+      const harnessMetadata = harnessStore(pinia);
 
-    // if a waypoint override was specified, use that
-    if (this.$attrs["harness-waypoint"]) {
-      waypoint = this.$attrs["harness-waypoint"];
-    }
+      if (
+        this.$route &&
+        this.$route.name &&
+        harnessMetadata.getPages.includes(this.$route.name)
+      ) {
+        waypoint = this.$route.name;
+      }
+      // if router is not installed and only a single harness page exists, use it as waypoint
+      if ((harnessMetadata.getPages.length === 1) & !this.$route) {
+        waypoint = harnessMetadata.pages[0];
+      }
 
-    if (waypoint) {
-      // get page store
-      const pageFunc = harnessMetadata.getPageStores[waypoint];
-      const pageDefinition = harnessMetadata.getpageDefinitions[waypoint];
-      const state = getAttributeNames(getState(pageDefinition));
-      const actions = getAttributeNames({
-        ...getActions(pageDefinition),
-        ...getValidationFunctions(),
-        ...getFilterHelperFunctions(),
-        ...getChartHelperFunctions(),
-        ...getDataHelperFunctions(),
-      });
-      this.$options.methods = {
-        ...this.$options.methods,
-        ...mapActions(pageFunc, actions),
-      };
-      this.$options.computed = {
-        ...this.$options.computed,
-        ...mapState(pageFunc, state),
-      };
-    }
-  },
-};
+      // if a waypoint override was specified, use that
+      if (this.$attrs["harness-waypoint"]) {
+        waypoint = this.$attrs["harness-waypoint"];
+      }
+
+      if (waypoint) {
+        // get page store
+        const pageFunc = harnessMetadata.getPageStores[waypoint];
+        // const pageStore = pageFunc();
+        const pageDefinition = harnessMetadata.getpageDefinitions[waypoint];
+        //   const pageDefinition = { key: "foo" };
+        const state = getAttributeNames(getState(pageDefinition));
+        const getters = getAttributeNames(getGetters(pageDefinition));
+        const actions = getAttributeNames({
+          ...getActions(pageDefinition),
+          ...getValidationFunctions(),
+          ...getFilterHelperFunctions(),
+          ...getChartHelperFunctions(),
+          ...getDataHelperFunctions(),
+        });
+        this.$options.methods = {
+          ...this.$options.methods,
+          ...mapActions(pageFunc, actions),
+        };
+        this.$options.computed = {
+          ...this.$options.computed,
+          ...mapState(pageFunc, state),
+        };
+      }
+    },
+  };
+}
 
 function getAttributeNames(obj) {
   return Object.getOwnPropertyNames(obj).filter(
