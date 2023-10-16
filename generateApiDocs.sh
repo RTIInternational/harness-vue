3d7cb7dad02acecc03e3dfc89c93fa706d2e62a4
Roman Ruiz-Esparza <2654709+rjruizes@users.noreply.github.com>
2023-10-16 14:35:04 -0400

    try CI w/o docker

--- generateApiDocs.sh
+++ generateApiDocs.sh
@@ -1,9 +1,8 @@
 rm -rf docs/api
 cp -R docs/api-stubs docs/api
 
-npx jsdoc2md src/store/charts.js --partial main.hbs >> docs/api/charts.md
-npx jsdoc2md src/store/filters.js --partial main.hbs >> docs/api/filters.md
-npx jsdoc2md src/store/data.js --partial main.hbs >> docs/api/data.md
-npx jsdoc2md src/store/actions.js --partial main.hbs >> docs/api/lifecycle.md
-npx jsdoc2md src/store/getters.js --partial main.hbs >> docs/api/getters.md
-echo "✅"
+jsdoc2md src/store/charts.js --partial main.hbs >> docs/api/charts.md
+jsdoc2md src/store/filters.js --partial main.hbs >> docs/api/filters.md
+jsdoc2md src/store/data.js --partial main.hbs >> docs/api/data.md
+jsdoc2md src/store/actions.js --partial main.hbs >> docs/api/lifecycle.md
+jsdoc2md src/store/getters.js --partial main.hbs >> docs/api/getters.md
