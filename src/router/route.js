1b898a5b4ed4dc018a9c6c260ef674ac5e964984
Alex Harding <alexharding@rti.org>
2024-01-23 09:23:32 -0500

    altering route to allow for developer control over route behaviors

--- src/router/route.js
+++ src/router/route.js
@@ -1,19 +1,21 @@
 export default function createHarnessRoute(store, pageDefinition) {
   return {
-    path: "/" + pageDefinition.key,
-    name: pageDefinition.key,
+    path: `/${pageDefinition?.routePath || pageDefinition.key}`,
+    name: pageDefinition?.routeName || pageDefinition.key,
     component: pageDefinition.pageComponent,
-    props: pageDefinition.pageProps ? pageDefinition.pageProps : false,
-    beforeEnter: pageDefinition.loadData
-      ? (to, from, next) => {
-          if (from.name) {
-            store.clearData();
-          }
-          if (to.name) {
-            store.loadData();
-          }
-          next();
+    props: pageDefinition?.pageProps,
+    beforeEnter: (to, from, next) => {
+      if (pageDefinition?.routeBeforeEnter) {
+        pageDefinition.routeBeforeEnter(to, from, next, store);
+      } else {
+        if (from.name) {
+          store.clearData();
         }
-      : null,
+        if (to.name) {
+          store.loadData();
+        }
+        next();
+      }
+    },
   };
 }
